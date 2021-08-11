3f20572ae1c7e0b5b42e67f1cce29b76384e474a
AHMEDRAZA\AhmedRaza <ahmad.raza@vicenna.com>
2021-08-11 11:57:28 +0500

    Added a controller named ProductController, wrote a Get method in it, then copied the connectionstring from App.Config and pasted it into Web.Config

--- DemoApp/DemoApp/Controllers/ProductController.cs
+++ DemoApp/DemoApp/Controllers/ProductController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ProductEntity;
 
 namespace DemoApp.Controllers
 {
     public class ProductController : ApiController
     {
+        public IEnumerable<Product> Get()
+        {
+            using (ProductEntities entity = new ProductEntities())
+            {
+                return entity.Products.ToList();
+            }
+        }
     }
 }
