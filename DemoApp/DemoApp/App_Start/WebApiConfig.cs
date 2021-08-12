09c1269d95bdeaae45aa46f20a3e47e3c2908b16
AHMEDRAZA\AhmedRaza <ahmad.raza@vicenna.com>
2021-08-12 15:17:23 +0500

    Added configuration for JSON/XML identation and camel-case formatter to the server in App_Start/WebApiConfig.cs

--- DemoApp/DemoApp/App_Start/WebApiConfig.cs
+++ DemoApp/DemoApp/App_Start/WebApiConfig.cs
@@ -14,6 +14,11 @@ namespace DemoApp
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            // configuration for JSON/XML indentation and camel-case format to the server
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            config.Formatters.XmlFormatter.Indent = true;
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
